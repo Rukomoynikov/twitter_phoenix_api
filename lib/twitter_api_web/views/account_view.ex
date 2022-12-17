86181068f800fdd6d8acdc153710881e2c0dfe0c
Max Rukomoynikov <max@apply4.com>
2022-12-17 22:33:04 +0000

    Signin up

--- lib/twitter_api_web/views/account_view.ex
+++ lib/twitter_api_web/views/account_view.ex
@@ -2,19 +2,7 @@ defmodule TwitterApiWeb.AccountView do
   use TwitterApiWeb, :view
   alias TwitterApiWeb.AccountView
 
-  def render("index.json", %{accounts: accounts}) do
-    %{data: render_many(accounts, AccountView, "account.json")}
-  end
-
-  def render("show.json", %{account: account}) do
-    %{data: render_one(account, AccountView, "account.json")}
-  end
-
-  def render("account.json", %{account: account}) do
-    %{
-      id: account.id,
-      email: account.email,
-      hashed_password: account.hashed_password
-    }
+  def render("sign_up.json", %{account: %{email: email}, token: token}) do
+    %{data: %{email: email, token: token}}
   end
 end
